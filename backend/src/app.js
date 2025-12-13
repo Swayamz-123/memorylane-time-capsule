05a518a1423cac87fce981827b8dc5f320251094
Swayamz-123 <itzswayam890@gmail.com>
2025-12-13 14:13:01 +0530

    feat: implement user authentication and authorization with JWT and production grade code with custom error handling and custom api  response to comfort the user

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 import express from "express"
 import cors from 'cors'
 import cookieParser from "cookie-parser"
+import userRouter from './routes/user.routes.js'
 const app = express()    
 app.use(cors({
     origin:process.env.CORS_ORIGIN, 
@@ -11,6 +12,6 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true ,limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
+app.use("/api/v1/users",userRouter)
 
 export {app}
