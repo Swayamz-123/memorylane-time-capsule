a4446c194aecd5c5b92109945b8a43e2fe817588
Swayamz-123 <itzswayam890@gmail.com>
2025-12-15 00:54:37 +0530

    made small change so that deployed site doesnt break

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -109,7 +109,7 @@ const loginUser = asyncHandler(async (req, res) => {
     .json(
       new ApiResponse(
         200,
-        { user: loggedInUser },
+        { user: loggedInUser,accessToken,refreshToken},
         "User logged in successfully"
       )
     );
