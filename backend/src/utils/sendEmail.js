626716e364584b6181b06f2c63a90af2c3adbe96
Swayamz-123 <itzswayam890@gmail.com>
2025-12-15 03:26:24 +0530

    debug

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const sendEmail = async ({ to, subject, text, html }) => {
   console.log("📧 Sending to:", to, "Subject:", subject.slice(0, 30));
   
-  const transporter = nodemailer.createTransporter({
+  const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST || "smtp.gmail.com",
     port: parseInt(process.env.EMAIL_PORT) || 587,
     secure: false, // true for 465, false for other ports
