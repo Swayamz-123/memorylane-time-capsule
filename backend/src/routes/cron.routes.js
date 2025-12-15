342f085bc10254d8bef119569a2dfca9d0c18605
Swayamz-123 <itzswayam890@gmail.com>
2025-12-15 10:45:46 +0530

    changed cron router from post to get

--- backend/src/routes/cron.routes.js
+++ backend/src/routes/cron.routes.js
@@ -3,6 +3,6 @@ import { unlockCapsulesCron } from "../controllers/cron.controller.js";
 
 const router = express.Router();
 
-router.post("/unlock-capsules", unlockCapsulesCron);
+router.get("/unlock-capsules", unlockCapsulesCron);
 
 export default router;
